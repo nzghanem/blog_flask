e706b3e8b5ae6a62c088cf1f99c8618f30ead218
aboghnez <nz.ghanem@gmail.com>
2023-01-22 14:59:12 +0100

    Video 2: Adds dummy data

--- flaskblog.py
+++ flaskblog.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Nezar Ghanem",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2018"
+    },
+    {
+        "author": "Leen Ghanem",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018"
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
